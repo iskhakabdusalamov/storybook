9b62f2e80ae50bb59a11deb91642240cd31a60af
Iskhak Abdusalamov <iskhak_abdusalamov@icloud.com>
2025-11-23 14:43:59 +0500

    setup eslint & prettier

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,10 +6,8 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
   ],
   "framework": {
     "name": "@storybook/vue3-vite",
