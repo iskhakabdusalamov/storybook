008ef3694060a2a8dcae495391a2f9e1fb3da19e
Iskhak Abdusalamov <iskhak_abdusalamov@icloud.com>
2025-12-06 18:42:48 +0500

    vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,8 +7,19 @@ export default mergeConfig(
   defineConfig({
     test: {
       environment: 'jsdom',
+      include: ['src/**/*.vi.test.ts'],
       exclude: [...configDefaults.exclude],
       root: fileURLToPath(new URL('./', import.meta.url)),
+
+      coverage: {
+        enabled: true,
+        provider: 'v8',
+        reporter: ['text', 'html'],
+        reportsDirectory: 'coverage',
+        include: ['src'],
+        exclude: ['src/**/*.stories.ts'],
+        all: true,
+      },
     },
   }),
 )
